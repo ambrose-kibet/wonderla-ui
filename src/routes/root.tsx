a8d723c28c991bad6088bedbedf9d2b820cebef6
ambrose-kibet <ambrosekibet36@gmail.com>
2025-07-29 22:21:44 +0300

    feat: add Root component with Navbar and Sidebar layout

---
+++ src/routes/root.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/sidebar";
+
+const Root: React.FC = () => {
+  return (
+    <div className="bg-blue-950 flex flex-col min-h-screen min-w-screen overflow-hidden">
+      <Navbar />
+      <Sidebar />
+      <div className="flex flex-col items-center justify-center flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+export default Root;
