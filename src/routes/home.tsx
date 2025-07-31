41c552d9677c74b48b520db95fc4a8825161838a
ambrose-kibet <ambrosekibet36@gmail.com>
2025-07-31 12:05:23 +0300

    fix: adjust layout in HomePage for consistent spacing and alignment

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,11 +3,11 @@ import RideCarousel from "../components/ride-carousel";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="grid grid-cols-[1fr_768px]  xl:grid-cols-[1fr_1020px] gap-4  my-auto w-full max-w-screen-2xl mx-auto">
-      <div className="col-span-1 overflow-hidden">
+    <div className="grid grid-cols-[1fr_768px]  xl:grid-cols-[1fr_1020px] gap-4 h-screen my-auto w-full max-w-screen-2xl mx-auto">
+      <div className="col-span-1 overflow-hidden my-auto">
         <CategorySidebar />
       </div>
-      <div className="col-span-1">
+      <div className="col-span-1 my-auto">
         <RideCarousel />
       </div>
     </div>
